c5271835350d77600585f77a96262785d8bccab2
Thomas Kolb <thomas@tkolb.de>
2019-05-06 14:28:49 +0200

    Added dependency install script for debian

---
+++ install_deps_debian.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+apt install \
+	g++ \
+	libeigen3-dev \
+	mariadb-server \
+	libmysqlcppconn-dev \
+	zlib1g-dev \
+	libpython3-dev \
+	libjpeg-dev \
+	docker \
+	cmake \
+	libssl-dev \
+	python3-venv \
+	libmariadbclient-dev
